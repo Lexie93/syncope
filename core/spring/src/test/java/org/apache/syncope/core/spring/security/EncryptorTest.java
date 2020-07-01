48cc8499aa15b86f4c007d1a3adeaa98f86a1b2a
Lexie <ponzo93@gmail.com>
2020-07-02 01:06:51 +0200

    using Enclosed runner for EncryptorTest

--- core/spring/src/test/java/org/apache/syncope/core/spring/security/EncryptorTest.java
+++ core/spring/src/test/java/org/apache/syncope/core/spring/security/EncryptorTest.java
@@ -3,71 +3,84 @@ package org.apache.syncope.core.spring.security;
 import org.apache.syncope.common.lib.types.CipherAlgorithm;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
+@RunWith(Enclosed.class)
 public class EncryptorTest{
 
     private static final Encryptor encryptor = Encryptor.getInstance("secretKeyLongEnough");
-    private static final String payload = "string to encode";
-    private static final String encryptedPayload = "wfvb5asUCaVke2twkQwb8KQmHDhdmm/xFHoOeLO+qzM=";
 
-    @Test
-    public void testEncode() {
-        try {
-            String encrypted = encryptor.encode(payload, CipherAlgorithm.AES);
-            assertEquals("bad encoding", encryptedPayload, encrypted);
-        } catch (Exception e){
-            Assert.fail("Should not throw Exception " + e);
+    @RunWith(Parameterized.class)
+    public static class parameterizedTests{
+
+        private final String message;
+        private final String encryptedMessage;
+
+        @Parameterized.Parameters
+        public static Collection<String[]> getParameters(){
+            return Arrays.asList(new String[][]{
+                    {"string to encode", "wfvb5asUCaVke2twkQwb8KQmHDhdmm/xFHoOeLO+qzM="},
+                    {null, null}
+            });
+        }
+
+        public parameterizedTests(String message, String encryptedMessage){
+            this.message = message;
+            this.encryptedMessage = encryptedMessage;
+        }
+
+        @Test
+        public void testEncode() {
+            try {
+                String encrypted = encryptor.encode(message, CipherAlgorithm.AES);
+                assertEquals("bad encoding", encryptedMessage, encrypted);
+            } catch (Exception e){
+                Assert.fail("Should not throw Exception " + e);
+            }
+        }
+
+        @Test
+        public void testDecode(){
+            try {
+                String decrypted = encryptor.decode(encryptedMessage, CipherAlgorithm.AES);
+                assertEquals("bad decoding", message, decrypted);
+            } catch (Exception e){
+                Assert.fail("Should not throw Exception " + e);
+            }
+        }
+
+        @Test
+        public void testVerify(){
+            if (message != null) {
+                assertTrue("false negative", encryptor.verify(message, CipherAlgorithm.AES, encryptedMessage));
+            } else {
+                //makes pitest throw an exception after mutation, but coverage is good
+                assertFalse("false positive", encryptor.verify(null, CipherAlgorithm.AES, encryptedMessage));
+            }
         }
     }
 
-    @Test
-    public void testEncodeNull() {
-        try {
-            String encrypted = encryptor.encode(null, CipherAlgorithm.AES);
-            assertNull("bad encoding", encrypted);
-        } catch (Exception e){
-            Assert.fail("Should not throw Exception " + e);
-        }
-    }
+    public static class notParameterizedTests{
 
-    //dubious but maybe still ok
-    @Test
-    public void testEncodeDecodeVoid() {
-        try {
-            String encrypted = encryptor.encode("", CipherAlgorithm.AES);
-            String decrypted = encryptor.decode(encrypted, CipherAlgorithm.AES);
-            assertEquals("bad encoding", "", decrypted);
-        } catch (Exception e){
-            Assert.fail("Should not throw Exception " + e);
+        //dubious but maybe still ok
+        @Test
+        public void testEncodeDecodeVoid() {
+            try {
+                String encrypted = encryptor.encode("", CipherAlgorithm.AES);
+                String decrypted = encryptor.decode(encrypted, CipherAlgorithm.AES);
+                assertEquals("bad encoding", "", decrypted);
+            } catch (Exception e){
+                Assert.fail("Should not throw Exception " + e);
+            }
         }
-    }
-    
-    @Test
-    public void testDecode(){
-        try {
-            String decrypted = encryptor.decode(encryptedPayload, CipherAlgorithm.AES);
-            assertEquals("bad decoding", payload, decrypted);
-        } catch (Exception e){
-            Assert.fail("Should not throw Exception " + e);
-        }
-    }
 
-    @Test
-    public void testDecodeNull(){
-        try {
-            String decrypted = encryptor.decode(null, CipherAlgorithm.AES);
-            assertNull("bad decoding", decrypted);
-        } catch (Exception e){
-            Assert.fail("Should not throw Exception " + e);
-        }
-    }
-
-    @Test
-    public void testVerify(){
-        assertTrue("false negative", encryptor.verify(payload, CipherAlgorithm.AES, encryptedPayload));
-        assertFalse("false positive", encryptor.verify(payload, CipherAlgorithm.AES, payload));
     }
 
 }
